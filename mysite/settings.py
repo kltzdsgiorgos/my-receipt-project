95ee783806359606c2fd29afc2966a92347c4143
Georgios Kalaitzidis <giorgoskltzds@gmail.com>
2022-09-10 02:40:54 +0300

    add models,admin

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'receipts.apps.ReceiptsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- receipts/models.py
+++ receipts/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+TAX = 0.24
+
+class Receipt(models.Model):
+
+    fill_date = models.DateTimeField('date filled')
+    fuel_type = models.CharField(max_length=10)
+    location = models.CharField(max_length=20)
+    gas_station = models.CharField(max_length=10)
+    price_per_liter = models.FloatField()
+    liters = models.FloatField()
+    gas_total_price = models.FloatField()
+
+    def __str__(self):
+        return str(self.fill_date) + self.fuel_type
+
+    def calculate_tax(self):
+
+        tax_price = self.gas_total_price * TAX
+        
+        return tax_price
+
+    def calculate_total_paid(self):
+
+        total_paid = self.tax_price + self.gas_total_price
+
+        return total_paid
