6340d5fab0178745740e6091f889f7130e2b85f1
Georgios Kalaitzidis <giorgoskltzds@gmail.com>
2022-09-11 01:54:25 +0300

    add requirements, views

--- receipts/urls.py
+++ receipts/urls.py
@@ -1,7 +1,8 @@
-from django.urls import re_path
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path = ('', views.index, name='index')
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
 ]
--- receipts/views.py
+++ receipts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Receipt
+
+class IndexView(generic.ListView):
+    template_name = 'receipts/index.html'
+    context_object_name = "receipts_list"
+
+    def get_queryset(self):
+        return Receipt.objects.all()
+
+class DetailView(generic.DetailView):
+    model = Receipt
+    template_name = 'receipts/detail.html'
