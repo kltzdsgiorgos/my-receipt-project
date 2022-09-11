1ec51e20979dd356a1c93b91d2729e32c2fb53e8
Georgios Kalaitzidis <giorgoskltzds@gmail.com>
2022-09-12 02:31:05 +0300

    working view, changed models

--- receipts/models.py
+++ receipts/models.py
@@ -11,18 +11,22 @@ class Receipt(models.Model):
     price_per_liter = models.FloatField()
     liters = models.FloatField()
     gas_total_price = models.FloatField()
+    tax_price = models.FloatField()
+    total_paid = models.FloatField()
 
+
+    # def __str__(self):
+    #     return str(self.fill_date) + " " + self.fuel_type
     def __str__(self):
-        return str(self.fill_date) + self.fuel_type
+        return str(self.pk)
+    # def calculate_tax(self):
 
-    def calculate_tax(self):
-
-        tax_price = self.gas_total_price * TAX
+    #     tax_price = self.gas_total_price * TAX
         
-        return tax_price
+    #     return tax_price
 
-    def calculate_total_paid(self):
+    # def calculate_total_paid(self):
 
-        total_paid = self.tax_price + self.gas_total_price
+    #     total_paid = self.tax_price + self.gas_total_price
 
-        return total_paid
+    #     return total_paid
